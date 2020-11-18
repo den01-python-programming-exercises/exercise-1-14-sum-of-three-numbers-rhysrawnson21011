72393412ef0d8bacfbf787b3d5d06117f74385b1
rhysrawnson2101 <Rhysrawnson@gmail.com>
2020-11-18 01:34:40 +0000

    three numbers with a twist

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,25 @@
 def main():
     #write your code below this line
 
+  first = int(input('Give the first number:'))
+  second = int(input('Give the second number:'))
+  third = int(input('Give the third number:'))
+  print('total:')
+  print(first + second + third)
+
+  fourth = int(input('Give the fourth number:'))
+  if fourth >100: 
+    print('thats mean why so high O_o')
+  fith = int(input('give the fith number:'))
+  if fourth + fith >100:
+    print('HiGhEr ;)')
+  sixth = int(input('give the sixth:'))
+  if sixth >100:
+    print('okay your just tring to get me to talk now')
+  
+  print('total:')
+  print(fourth + fith + sixth)
+  if fourth + fith + sixth >5000:
+    print('mAkInG mY bRaIn HuRt')
 if __name__ == '__main__':
     main()
